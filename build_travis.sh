57c16b1a87e0365825f90fcbc5b907ea217c9a7b
QiumingLu <cming_cs@163.com>
2017-03-04 22:00:23 +0800

    fix protobuf version

--- build_travis.sh
+++ build_travis.sh
@@ -2,7 +2,24 @@ CXX=g++
 CXXFLAGS="-std=c++11 -pthread -I/usr/local/include -I/usr/include"
 CXXOUTPUT="/tmp/skywalker_build_detect_platform-cxx.$$"
 
-# Test whether LevelDB library is installed
+# Test whether Protobuf library is installed
+$CXX $CXXFLAGS -x c++ - -o $CXXOUTPUT 2>/dev/null  <<EOF
+    #include <google/protobuf/arena.h>
+    int main() {}
+EOF
+  if [ "$?" != 0 ]; then
+    wget https://github.com/google/protobuf/archive/v3.2.0.tar.gz
+    tar zxvf v3.2.0.tar.gz
+    cd protobuf-3.2.0
+    ./autogen.sh
+    ./configure --prefix=/usr
+    make
+    sudo make install
+    cd ..
+    rm -rf protobuf-3.2.0
+    rm -rf v3.2.0.tar.gz
+  fi
+
 $CXX $CXXFLAGS -x c++ - -o $CXXOUTPUT 2>/dev/null  <<EOF
     #include <leveldb/db.h>
     int main() {}
@@ -38,3 +55,5 @@ EOF
     rm -rf voyager-1.6
     rm -rf v1.6.tar.gz
   fi
+
+rm -f $CXXOUTPUT
