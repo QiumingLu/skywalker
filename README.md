194506b4fd045c128f11b35512a5c76f673e38b5
QiumingLu <mirantslu@gmail.com>
2017-12-20 21:51:53 +0800

    Release v1.0.2

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ Skywalker支持Linux，Mac OS X，FreeBSD等类Unix平台，不支持Windows平�
 ## 编译依赖
 * LevelDB  v1.3及以上版本
 * Protobuf v3.0.0及以上版本
-* Voyager  v1.0.0及以上版本
+* Voyager  v1.0.2及以上版本
 
 ## 编译安装
 (1) LevelDB编译安装(https://github.com/google/leveldb/blob/master/README.md) 
--- include/skywalker/node.h
+++ include/skywalker/node.h
@@ -16,7 +16,7 @@ namespace skywalker {
 // Update Makefile if you change these
 static const int kMajorVersion = 1;
 static const int kMinorVersion = 0;
-static const int kPatchVersion = 1;
+static const int kPatchVersion = 2;
 
 // A Node is safe for concurrent access from multiple threads without
 // any external synchronization.
